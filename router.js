7b257edf792de17d598a7d386c98d5d1bec4be39
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-23 00:23:39 -0500

    setup authentication

--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+// using jwt strategy, when use is authenticated, dont try to create
+// a cookie-based session
+
 
 module.exports = function(app){
   app.post('/signup', Authentication.signup);
 
-  app.get('/', function(req, res, next){
+  app.get('/', requireAuth, function(req, res){
     res.send("Hello client");
   })
 }
